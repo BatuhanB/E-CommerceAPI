05bb7842af354b2536cbbabeb3e4de55d0958d05
BatuhanB <balibatuhan19@gmail.com>
2023-07-16 16:55:52 +0300

    Fluent Validation - Validation Filter

---
+++ Infrastructure/Commerce.Infrastructure/Filters/ValidationFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Commerce.Infrastructure.Filters
+{
+    public class ValidationFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errs = context.ModelState.Where(x => x.Value.Errors.Any())
+                    .ToDictionary(e => e.Key, e => e.Value.Errors.Select(x => x.ErrorMessage)).ToArray();
+                context.Result = new BadRequestObjectResult(errs);
+                return;
+            }
+            await next();
+        }
+    }
+}
--- Presentation/Commerce.API/Program.cs
+++ Presentation/Commerce.API/Program.cs
@@ -1,10 +1,15 @@
+using Commerce.Application.Validators.Products;
+using Commerce.Infrastructure.Filters;
 using Commerce.Persistence.Extensions;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var myCors = "_myCors";
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt => opt.Filters.Add<ValidationFilter>())
+    .AddFluentValidation(conf => conf.RegisterValidatorsFromAssemblyContaining<AddProductValidator>())
+    .ConfigureApiBehaviorOptions(opt => opt.SuppressModelStateInvalidFilter = true).AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
